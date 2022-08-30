cb748bdd0a681852d758b83e2e7ae8ccb7175b1d
yohannes-taye <yohannestaye83@gmail.com>
2022-08-30 13:57:24 +0800

    Remove run.sh helper file

--- src/run.sh
+++
@@ -1,6 +0,0 @@
-# cmake -DCMAKE_PREFIX_PATH=./libtorch ..
-cmake --build ./build --config Release
-./build/main \
-    /home/tmc/project/DPT_with_onnx_export/DPT/traced_model.pt \
-   /home/tmc/Documents/Data/depth_test_data/A011_04010827_C017/000206.jpg \
-   /home/tmc/Documents/Data/depth_test_data/A011_04010827_C017
